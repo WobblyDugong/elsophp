30173c654913c4b5a131db43a1f73cba5383c463
WobblyDugong <banhidibotond@gmail.com>
2022-03-22 13:57:03 +0100

    első kezdése

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP első oldalam</title>
+    <meta name="author" content="Bánhidi Botond">
+</head>
+
+<body>
+    <?php
+        echo "<h1>szia!</h1>";
+    ?>
+    
+    <p>
+    <?php
+        print("szia!");
+    ?>
+    </p>
+</body>
+
+</html>
